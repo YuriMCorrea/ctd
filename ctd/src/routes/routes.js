82534723969e061400e6485391a1869801f39322
IgorFMouta <89466547+IgorFMouta@users.noreply.github.com>
2021-12-14 23:09:05 -0300

    Inclusão de Routes

---
+++ ctd/src/routes/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Listar  from "../screens/Listar";
+// import { Home } from "../screens/home"
+
+
+const Stack = createStackNavigator();
+export const Routes = () =>{
+    return(
+        <Stack.Navigator screenOptions={{ headerTransparent: true, headerShown: false }}>
+            <Stack.Screen name="Listar" component={Listar} />
+            {/* <Stack.Screen name="Home" component={Home} /> */}
+        </Stack.Navigator>
+    )
+}
