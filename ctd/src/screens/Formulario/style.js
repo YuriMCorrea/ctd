cabf3d9cad48c6799451791c741f450ccdb90870
KelvinKSP <kelvinpenha10@gmail.com>
2021-12-14 23:20:27 -0300

    Formulario 14/12 - 23:15

---
+++ ctd/src/screens/Formulario/style.js
@@ -0,0 +1,56 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+
+const styles = EStyleSheet.create({
+    container: {
+        backgroundColor: '#838383',
+        width: "100%",
+        marginTop: "50%",
+        
+    },
+    containerTema: {
+        marginTop: "10%",
+        marginBottom: "10%",
+    },
+    nomeTema: {
+        fontSize: '1.5rem',
+        color: "#FFF",
+        textAlign: 'center',
+    },
+    form: {
+        alignItems: "center",
+        marginBottom: "10%",
+    },
+    nomeItem: {
+        fontSize: '1rem', 
+        color: "#FFF",
+        marginBottom: "2%",
+        marginTop: "2%",
+    },
+    textInput: {
+        backgroundColor: '#FFF',
+        width: "80%",
+    },
+    containerBotao: {
+        alignItems: "center",
+        marginTop: "10%",    
+    },
+    botao: {
+        width: "70%",
+        height: "20%",
+        backgroundColor: '#474747',
+        alignItems: "center",
+        marginBottom: "10%",
+        borderRadius: 10,
+    },
+    textoBotao: {
+        textAlign: 'center',
+    },
+
+
+
+
+
+});
+
+export default styles;
