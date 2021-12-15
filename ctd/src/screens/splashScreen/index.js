ae9b9d46cdf4ec4839ac44142e0aa1adff3e6c54
IgorFMouta <89466547+IgorFMouta@users.noreply.github.com>
2021-12-14 22:57:19 -0300

    Splashscreen

--- ctd/src/screens/splashScreen/index.js
+++ ctd/src/screens/splashScreen/index.js
@@ -1,24 +1,8 @@
 import api from "../../services"
 
-const axios = require('axios');
-
-// Make a request for a user with a given ID
-api.get('/user?ID=12345')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
 function deleteProduto (id){
     const response = api.delete(`/produto/${id}`)
     .then(function(response){
         alert(`${response}`);
     })
 }
-
